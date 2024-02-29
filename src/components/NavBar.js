81139601167dc21ed656490e4382a517939df773
Vishnu Vinod <vishnuvinod1205@gmail.com>
2024-02-29 20:54:42 +0530

    Changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = () => {
 
   return (
     <nav className="nav-bar">
-      <h1>React Chat</h1>
+      <h1>ChatHub</h1>
       {user ? (
         <button onClick={signOut} className="sign-out" type="button">
           Sign Out
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,9 +12,9 @@ const Welcome = () => {
 
   return (
     <main className="welcome">
-      <h2>Welcome to React Chat.</h2>
-      <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
-      <p>Sign in with Google to chat with with your fellow React Developers.</p>
+      <h2>Welcome to ChatHub.</h2>
+      <img src="/195.png" alt="ReactJs logo" width={300} height={300} />
+      <p>Sign in with Google to chat with friends.</p>
       <button className="sign-in">
         <img
           onClick={googleSignIn}
